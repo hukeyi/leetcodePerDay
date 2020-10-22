4ed0621332a6d3ccf10bbf891388ee83e555916a
hukeyi <hky1998@outlook.com>
2020-10-22 23:52:44 +0800

    day 4

---
+++ 283.移动零.js
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=javascript
+ *
+ * [283] 移动零
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    // return mySolution(nums);
+    // return goodSolution(nums);
+    // return goodSolution2(nums);
+    return goodSolution3(nums);
+};
+
+// 97.13 21.93
+var goodSolution3 = function (nums){
+    const len = nums.length;
+    let j = 0;
+    for (let i = 0; i < len; i++){
+        if (nums[i] !== 0){
+            // let temp = nums[i];
+            // nums[i] = nums[j];
+            // nums[j++] = temp;
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            j++;
+        }
+    }
+}
+
+// 84.78 21.09
+var goodSolution = function(nums){
+    const len = nums.length;
+    let j = 0;
+    for (let i = 0; i < len; i++){
+        if (nums[i] !== 0){
+            nums[j] = nums[i];
+            if (i !== j){
+                nums[i] = 0;
+            }
+            j++;
+        }
+    }
+}
+
+// 71.5 5.8
+var goodSolution2 = function (nums){
+    const len = nums.length;
+    let j = 0;
+    for (let i = 0; i < len; i++){
+        if (nums[j] === 0){
+            nums.splice(j, 1);
+            nums[len - 1] = 0
+        }else{
+            j++;
+        }
+    }
+}
+
+// 93.21 15.55
+var mySolution = function (nums) {
+    const len = nums.length;
+    let countZero = 0;
+    nums.forEach( num => {
+        if (num === 0) countZero++;
+    });
+    const countNotZero = len - countZero;
+    let pos = 0;
+    for (let i = 0; i < len; i++){
+        if (nums[i] !== 0){
+            nums[pos++] = nums[i];
+        }
+        if (pos >= countNotZero){
+            while(pos < len){
+                nums[pos++] = 0;
+            }
+            break;
+        }
+    }
+    return nums;
+}
+// @lc code=end
+
