2b431e2519a56f3c3f7ef27a3c1057e441fff592
hukeyi <hky1998@outlook.com>
2020-11-05 14:45:23 +0800

    day 18

---
+++ 236.二叉树的最近公共祖先.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=236 lang=javascript
+ *
+ * [236] 二叉树的最近公共祖先
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    // root不存在或 root就是p或 root就是q 则结果就是root
+    if (!root || root === p || root === q){
+        return root;
+    }
+    let left = lowestCommonAncestor(root.left, p, q);
+    let right = lowestCommonAncestor(root.right, p, q);
+    if (!left) return right;
+    if (!right) return left;
+    return root;
+};
+// @lc code=end
+
