22713dad89b1cae38d835c53a68d9dc977acc762
hukeyi <hky1998@outlook.com>
2020-12-03 09:58:05 +0800

    day 45

--- 200.岛屿数量.js
+++ 200.岛屿数量.js
@@ -9,31 +9,37 @@
  * @param {character[][]} grid
  * @return {number}
  */
-var numIslands = function(grid) {
-    return mysolution1(grid);
+var numIslands = function (grid) {
+	return solution1(grid);
 };
 
-var mysolution1 = function(grid){
-    let count = 0;
-    const dfs = function (grid, i, j){
-        if (i >= grid.length || j >= grid[0].length || i < 0 || j < 0 || grid[i][j] === '0'){
-            return;
-        }
-        grid[i][j] = '0';
-        dfs(grid, i, j + 1);
-        dfs(grid, i + 1, j);
-        dfs(grid, i - 1, j);
-        dfs(grid, i, j - 1);
-    }
-    for (let i = 0; i < grid.length; i++){
-        for (let j = 0; j < grid[0].length; j++){
-            if (grid[i][j] === '1'){
-                dfs(grid, i, j);
-                count++;
-            }
-        }
-    }
-    return count;
-}
+var solution1 = function (grid) {
+	let count = 0;
+	const dfs = function (grid, i, j) {
+		// terminator: if i, j exceeds the grid or the element is water
+		if (
+			i >= grid.length ||
+			j >= grid[0].length ||
+			i < 0 ||
+			j < 0 ||
+			grid[i][j] === '0'
+		) {
+			return;
+		}
+		grid[i][j] = '0';
+		dfs(grid, i + 1, j);
+		dfs(grid, i, j + 1);
+		dfs(grid, i - 1, j);
+		dfs(grid, i, j - 1);
+	};
+	for (let i = 0; i < grid.length; i++) {
+		for (let j = 0; j < grid[0].length; j++) {
+			if (grid[i][j] === '1') {
+				dfs(grid, i, j);
+				count++;
+			}
+		}
+	}
+	return count;
+};
 // @lc code=end
-
