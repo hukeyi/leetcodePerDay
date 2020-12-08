9536bcab5775f67738e85dea24c51ee984a10d17
hukeyi <hky1998@outlook.com>
2020-12-08 21:59:40 +0800

    day 51

--- 367.有效的完全平方数.js
+++ 367.有效的完全平方数.js
@@ -9,32 +9,30 @@
  * @param {number} num
  * @return {boolean}
  */
-var isPerfectSquare = function(num) {
-    return mysolution2(num);
+var isPerfectSquare = function (num) {
+	return solution1(num);
 };
 
-// binary search
-var mysolution1 = function(num){
-    if (num < 2) return true;
+var solution1 = function (num) {
+	if (num < 2) return num;
 
-    let [left, right] = [2, num];
-    while (left <= right){
-        const mid = left + Math.floor((right - left) / 2);
-        if (mid * mid === num) return true;
-        else if (mid * mid < num) left = mid + 1;
-        else right = mid - 1;
-    }
-    return false;
-}
+	let [left, right] = [2, num];
+	while (left <= right) {
+		const mid = left + Math.floor((right - left) / 2);
+		if (mid * mid === num) return true;
+		else if (mid * mid < num) left = mid + 1;
+		else right = mid - 1;
+	}
+	return false;
+};
 
 // newton
-var mysolution2 = function(num){
-    if (num < 2) return true;
-    let curr = num;
-    while (curr * curr > num){
-        curr = Math.floor((curr + num / curr) / 2);
-    }
-    return curr * curr === num;
-}
+var mysolution2 = function (num) {
+	if (num < 2) return true;
+	let curr = num;
+	while (curr * curr > num) {
+		curr = Math.floor((curr + num / curr) / 2);
+	}
+	return curr * curr === num;
+};
 // @lc code=end
-
